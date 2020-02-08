c52978a12f14cd16dfba8a9aa3bb96d59d9326e6
mariolsun <mariolsun@yandex.ru>
2020-02-09 01:41:29 +0300

    Updates

---
+++ script/CardList.js
@@ -0,0 +1,31 @@
+class CardList {
+  constructor(domElement) {
+    this.container = domElement;
+    this.cards = [];
+  }
+
+  render(cards) {
+    cards.forEach((card) => {
+      this.addCard(card);
+    });
+  }
+
+  addCard(card) {
+    this.cards.push(card);
+    this.container.appendChild(card.elem);
+  }
+
+  findCard(cardElem) {
+    let res;
+    this.cards.forEach(card => {
+      if(card.elem == cardElem) {res = card;}
+     
+    });
+    return res;
+  }
+
+  deleteCard(card) {
+    this.container.remove(card.elem);
+  }
+
+}
---
+++ script/token.js
@@ -0,0 +1,3 @@
+
+const token = '84c86aad-d0d1-4ec9-91d9-06e6853e2cef';
+const userId = 'ea1bff16927c3a7237205010';
